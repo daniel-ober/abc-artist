7e07b6df79d9f3071c03efba4fe72dfe927b29fe
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-01-26 16:00:54 -0600

    pseudocoding, core html, basic css

---
+++ app.js
@@ -0,0 +1,52 @@
+// API URL: https://www.theaudiodb.com/api/v1/json/1/search.php?s={Artist name}
+let artistName = 'madonna'
+const domain = 'https://www.theaudiodb.com'
+const url = `${domain}/api/v1/json/1/search.php?s=${artistName}`
+
+
+
+
+    /*    */    
+    /*    */
+    /*  declare variable to reteieve API data */
+    /*    */
+    /*    */
+    /*  setup try...catch for data retrieval */
+    /*    */    
+    /*    */
+    /*  append data results to the DOM  */  
+    /*    */   
+    /*    */
+    /*  declare variable for data  */
+    /*    */
+    /*    */    
+    /*  setup my form  */
+    /*    */    
+    /*    */
+    /*  capture user input  */
+    /*    */
+    /*    */ 
+    /*  display data (artist name, genre, photo, bio)  */
+    /*    */
+    /*    */
+    /*  reset DOM  */    
+    /*    */
+    /*    */
+    /*    */
+    /*    */
+    /*    */
+    /*    */
+    /*    */
+    /*    */    
+    /*    */
+    /*    */
+    /*    */
+    /*    */ 
+    /*    */
+    /*    */
+    /*    */ 
+    /*    */    
+    /*    */
+    /*    */
+    /*    */
+    /*    */               
