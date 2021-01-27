cb4d54a04c17a2038009fd3d57a1369ec800bf3c
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-01-27 09:10:25 -0600

    async setup; retrieving api data successfully

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
-// API URL: https://www.theaudiodb.com/api/v1/json/1/search.php?s={Artist name}
-let artistName = 'madonna'
 const domain = 'https://www.theaudiodb.com'
-const url = `${domain}/api/v1/json/1/search.php?s=${artistName}`
+const url = `${domain}/api/v1/json/1/search.php?s=`
 
+const getData = async () => {  /*  declare var to pull API data async */  
+    try {  /*  setup try...catch for the data retrieval */
+    let response = await axios.get(url) /* declare variable that will call on broad data set */
+    // console.log(response) 
+    let artistData = response.data.artists[0] /* declare variable that points to our data set which contains data for an artist */
+    console.log(artistData)
+    } catch (error) { /*  catch setup as a fallback to catch an error in response */
+        console.log(error)
+    }
+}
+getData()
 
-
-
-    /*    */    
-    /*    */
-    /*  declare variable to reteieve API data */
-    /*    */
-    /*    */
-    /*  setup try...catch for data retrieval */
-    /*    */    
-    /*    */
-    /*  append data results to the DOM  */  
-    /*    */   
-    /*    */
-    /*  declare variable for data  */
-    /*    */
-    /*    */    
-    /*  setup my form  */
-    /*    */    
-    /*    */
-    /*  capture user input  */
-    /*    */
-    /*    */ 
-    /*  display data (artist name, genre, photo, bio)  */
-    /*    */
-    /*    */
-    /*  reset DOM  */    
-    /*    */
-    /*    */
-    /*    */
-    /*    */
-    /*    */
-    /*    */
-    /*    */
-    /*    */    
-    /*    */
-    /*    */
-    /*    */
-    /*    */ 
-    /*    */
-    /*    */
-    /*    */ 
-    /*    */    
-    /*    */
-    /*    */
-    /*    */
-    /*    */               
+/*  pull button via querySelector  */
+/*    */
+/*    */    
+/*  set event listener to prevent data from showing until click  */
+/*    */
+/*    */    
+/*  declare a variable that puts user's value input into the 'blank' id of header  */
+/*    */
+/*    */
+/*  create a function called artistInfo that pulls artist name, genre, photo from the data from our try...let
+/*    */
+/*    */   
+/*  displays the previous data on the browser. Tip: declare a variable within the fuctions that pulls from each of the data areas needed (ie. `<img src='${artistData.thumbnail}> <h1>${artistName.artist}</h1> and <h2>${artistName.genre}</h2>) */ 
+/*    */
+/*    */   
+/*  return artistInfo to '.artist-section' */    
+/*    */
+/*    */
+/*  create a function called artistInfo that pulls artist biography from the data from our try...let
+/*    */
+/*    */   
+/*  displays the previous data on the browser. Tip: declare a variable within the fuctions that pulls from each of the data areas needed (ie. `<img srcs='${artistData.thumbnail}> <h1>${artistName.artist}</h1> and <h2>${artistName.genre}</h2>) */ /*  return artistInfo to 'bio-section' */
+/*    */
+/*    */
+/*  reset DOM  */
+/*    */
+/*    */    
