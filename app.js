8b0c520e3cb8887d16fb947a29241fd43ad861ef
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-01-27 10:56:28 -0600

    setup and tested search button and user value input: successfully able to console log unique artist from browser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// STEP 1: API URL INFO
+
 const domain = 'https://www.theaudiodb.com'
 const url = `${domain}/api/v1/json/1/search.php?s=`
 
 
-async function searchArtist() {  /*  create an async function that points to music artist data */  
+// STEP 2: ACCESS OUR API DATA
+
+async function searchArtists(uniqueArtist) {  /*  create an async function that points to music artist data */  /* set my function parameter to 'uniqueArtist' once I got to STEP 3, "declare a variable that puts user's value input into the 'blank' id of header" */
     try {  /*  setup try for the data retrieval */
-    let response = await axios.get(url) /* declare variable that will call on the data from our API url above */
-    // console.log(response) 
+    let response = await axios.get(`${url}${uniqueArtist}`) /* declare variable that will call on the data from our API url above */ /* added 'uniqueArtist' as the url endpoint once I got to STEP 3, "declare a variable that puts user's value input into the 'blank' id of header" */
+    console.log(response) 
     let artistData = response.data.artists[0] /* declare variable that points to the specific data set that contains information for music artists */
     console.log(artistData)
     } catch (error) { /*  catch setup as a fallback to in case of an error in our response var */
@@ -13,21 +17,24 @@ async function searchArtist() {  /*  create an async function that points to mus
     }
 }
 
+
+// STEP 3: SETUP FUNCTIONALITY FOR UNIQUE SEARCH FROM THE BROWSER
+
 const searchButton = document.querySelector('#search') /*  declare a var that pulls our search button via querySelector  */
-console.log(searchButton) /* tested via console and got successful response: <button id="search">Search</button> */
+// console.log(searchButton) /* tested via console and got successful response: <button id="search">Search</button> */
 
 
-/*  set event listener to prevent data from showing until click  */
-searchButton.addEventListener('click', (e) => { 
+searchButton.addEventListener('click', (e) => { /*  set event listener to prevent data from showing until click  */
     e.preventDefault()
     // console.log(searchButton) /* not sure if there's a right way to test this. previously tested via console and got successful response: <button id="search">Search</button> */
-})
-
-searchArtist()
- 
 
 
 /*  declare a variable that puts user's value input into the 'blank' id of header  */
+const artistValue = document.querySelector('#blank').value
+    searchArtists(artistValue) /* sucessfully tested input on browser: entered an artist name, clicked 'search' button, console log displayed  */
+})
+// searchArtists() /* un-commented searchArtist()  once I was able to get the unique artist data in Chrome dev tool. */
+
 
 
 
