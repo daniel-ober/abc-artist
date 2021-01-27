09b79619bf8152b2f4164ede5b3534b2af9b8417
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-01-27 12:15:47 -0600

    created function to display targeted data on browser (in progress)

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
-// STEP 1: API URL INFO
+// COMPONENT 1: DECLARE VARIABLES FOR API URL INFO
 
 const domain = 'https://www.theaudiodb.com'
-const url = `${domain}/api/v1/json/1/search.php?s=`
+const api = `/api/v1/json/1/search.php?s=`
+const url = `${domain}${api}`
 
 
-// STEP 2: ACCESS OUR API DATA
 
-async function searchArtists(uniqueArtist) {  /*  create an async function that points to music artist data */  /* set my function parameter to 'uniqueArtist' once I got to STEP 3, "declare a variable that puts user's value input into the 'blank' id of header" */
+// COMPONENT 2: ACCESS API DATA
+
+async function getData(uniqueArtist) {  /*  create an async function that points to music artist data */  /* set my function parameter to 'uniqueArtist' once I got to STEP 3, "declare a variable that puts user's value input into the 'blank' id of header" */
     try {  /*  setup try for the data retrieval */
     let response = await axios.get(`${url}${uniqueArtist}`) /* declare variable that will call on the data from our API url above */ /* added 'uniqueArtist' as the url endpoint once I got to STEP 3, "declare a variable that puts user's value input into the 'blank' id of header" */
-    console.log(response) 
-    let artistData = response.data.artists[0] /* declare variable that points to the specific data set that contains information for music artists */
-    console.log(artistData)
+    // console.log(response) 
+    let artistDataSet = response.data.artists[0] /* declare variable that points to the specific data set that contains information for music artists */
+    console.log(artistDataSet)
     } catch (error) { /*  catch setup as a fallback to in case of an error in our response var */
         console.log(error)
     }
 }
 
 
-// STEP 3: SETUP FUNCTIONALITY FOR UNIQUE SEARCH FROM THE BROWSER
+// COMPONENT 3: SETUP FUNCTIONALITY FOR UNIQUE SEARCH FROM THE BROWSER
 
 const searchButton = document.querySelector('#search') /*  declare a var that pulls our search button via querySelector  */
 // console.log(searchButton) /* tested via console and got successful response: <button id="search">Search</button> */
-
 
 searchButton.addEventListener('click', (e) => { /*  set event listener to prevent data from showing until click  */
     e.preventDefault()
     // console.log(searchButton) /* not sure if there's a right way to test this. previously tested via console and got successful response: <button id="search">Search</button> */
 
-
-/*  declare a variable that puts user's value input into the 'blank' id of header  */
-const artistValue = document.querySelector('#blank').value
-    searchArtists(artistValue) /* sucessfully tested input on browser: entered an artist name, clicked 'search' button, console log displayed  */
+const artistValue = document.querySelector('#blank').value /*  declare a variable that puts user's value input into the 'blank' id of header  */
+    getData(artistValue) /* sucessfully tested input on browser: entered an artist name, clicked 'search' button, console log displayed  */
 })
-// searchArtists() /* un-commented searchArtist()  once I was able to get the unique artist data in Chrome dev tool. */
+// getData() /* un-commented getData()  once I was able to get the unique artist data in Chrome dev tool. */
 
 
 
+// COMPONENT 4: DISPLAY OUR SEARCH IN THE BROWSER FOR THE USER
 
-/*  create a function called artistInfo that pulls artist name, genre, photo from the data from our try...let
- 
+/*  create a function called that pulls data from our artistDataSet variable and then displays that data onto the browser. Target the following endpoints: strArtist (artist name), strGenre (genre), strArtistThumb (thumbnail photo), strArtistLogo (logo), strBiographyEN (bio), strWebsite (website), strFacebook (facebook), strTwitter (twitter) */
+ function artistDetails(artistDataSet) {
+     let artistInfo = 
+    `
+    <h1>${artistInfo.strArtist}</h1>
+    <h2>${artistInfo.strGenre}</h2>
+    <img src='${artistInfo.strArtistThumb}'></img src>
+    <img src='${artistInfo.strArtistLogo}'></img src>
+    <h1>${artistInfo.strBiographyEN}</h1>
+    <a href='${artistInfo.strWebsite}'></a href>
+    <a href='${artistInfo.strFacebook}'></a href>
+    <a href='${artistInfo.strTwitter}'></a href>
+    `
+}
 
 
-/*  displays the previous data on the browser. Tip: declare a variable within the fuctions that pulls from each of the data areas needed (ie. `<img src='${artistData.thumbnail}> <h1>${artistName.artist}</h1> and <h2>${artistName.genre}</h2>) */ 
+/*  displays the previous data on the browser. Tip: declare a variable within the fuctions that pulls from each of the data areas needed (ie. `<img src='${artistDataSet.thumbnail}> <h1>${artistName.artist}</h1> and <h2>${artistName.genre}</h2>) */ 
  
 
 
@@ -54,7 +66,7 @@ const artistValue = document.querySelector('#blank').value
 
 
 
-/*  displays the previous data on the browser. Tip: declare a variable within the fuctions that pulls from each of the data areas needed (ie. `<img srcs='${artistData.thumbnail}> <h1>${artistName.artist}</h1> and <h2>${artistName.genre}</h2>) */ /*  return artistInfo to 'bio-section' */
+/*  displays the previous data on the browser. Tip: declare a variable within the fuctions that pulls from each of the data areas needed (ie. `<img srcs='${artistDataSet.thumbnail}> <h1>${artistName.artist}</h1> and <h2>${artistName.genre}</h2>) */ /*  return artistInfo to 'bio-section' */
 
 
 
