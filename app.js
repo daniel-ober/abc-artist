875caf631308c625161eec1d420f4f8f052d5079
Daniel Ober <ChillDrummer@MacBook-Pro.hsd1.tn.comcast.net>
2021-01-29 10:43:54 -0600

    cleaned up my containers, getting rid of 33 lines of unnecessary code.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const api = `/api/v1/json/1/search.php?s=`
 const url = `${domain}${api}`
 
 
-
 // COMPONENT 2: ACCESS API DATA
 async function getData(uniqueArtist) {  /*  create an async function that points to music artist data */  /* set my function parameter to 'uniqueArtist' once I got to STEP 3, "declare a variable that puts user's value input into the 'blank' id of header" */
     try {  /*  setup try for the data retrieval */
@@ -13,15 +12,10 @@ async function getData(uniqueArtist) {  /*  create an async function that points
         // console.log(response) 
     let data = response.data.artists[0] /* declare variable that points to the specific data set that contains information for music artists */
         // console.log(data)
-    removeBio()
-    artistDetails(data)
-    artistBio(data)
-    removeThumbnail()
-    removeBgImg()
-    removeDetails()
-    artistBgImg(data)
-    artistThumbnail(data)
-    artistDetails(data)
+        removeMain()
+        removeBio()
+        searchMain(data)
+        searchBio(data)
     } catch (error) { /*  catch setup as a fallback to in case of an error in our response var */
         console.log(error)
     }
@@ -43,45 +37,29 @@ const artistValue = document.querySelector('#blank').value /*  declare a variabl
 
 
 // COMPONENT 4: APPEND ARTIST DATA TO DOM
-function artistBgImg(data) { /*  add artist bg image to .main-container */
-    let mainArt = 
+function searchMain(data) { /*  add artist bg image to .main-container */
+    let artistMain = 
     `
-    <img class='background-image' src='${data.strArtistFanart}'></img>
+    <img src='${data.strArtistFanart}' id='fan-art'></img>
+    <div class='artist-container'>
+        <img src='${data.strArtistThumb}' alt='${data.strArtist}' id='artist-thumbnail'></img>
+        <div class='artist-details'>
+            <img src='${data.strArtistLogo}' alt='logo' id='artist-logo'></img>
+            <a class='social-media' href='https://${data.strWebsite}' target='_blank'>Website</a>
+            <a class='social-media' href='https://${data.strFacebook}' target='_blank'>Facebook</a>
+            <a class='social-media' href='https://${data.strTwitter}' target='_blank'>Twitter</a>
+        </div>
+    </div>
     `
     const mainContainer = document.querySelector('.main-container') /* return artistInfo to the body's '.main-container' */
-    mainContainer.insertAdjacentHTML('beforeend', mainArt)
-    return mainArt
+    mainContainer.insertAdjacentHTML('beforeend', artistMain)
+    return artistMain
 }
 
-function artistDetails(data) { /*  add artist details to .details */
-    let mainInfo = 
-    `
-    <img class='artist-logo' src='${data.strArtistLogo}' alt='logo'></img>
-    <h1 class='artist-name'>${data.strArtist}</h1>
-    <h2 class='artist-genre'>${data.strGenre}</h2>
-    <a href='https://${data.strWebsite}' target='_blank'>Website</a>
-    <a href='https://${data.strFacebook}' target='_blank'>Facebook</a>
-    <a href='https://${data.strTwitter}' target='_blank'>Twitter</a>
-    `
-    const infoContainer = document.querySelector('.details-container')
-    infoContainer.insertAdjacentHTML('beforeend', mainInfo)
-    return mainInfo
-}
-
-function artistThumbnail(data) { /*  add artist thumbnail to .artist-container */
-    let artThumbnail = 
-    `
-    <img class='artist-thumbnail' src='${data.strArtistThumb}' alt='${data.strArtist}'></img>
-    `
-    const thumbnailContainer = document.querySelector('.details-container') 
-    thumbnailContainer.insertAdjacentHTML('beforeend', artThumbnail)
-    return artThumbnail
-}
-
-function artistBio(data) { /*  add artist bio to .bio-container */
+function searchBio(data) { /*  add artist bio to .bio-container */
     let bioInfo =
     `
-    <p class='artist-bio'>${data.strBiographyEN}</p>
+    <p>${data.strBiographyEN}</p>
     `
     const bioContainer = document.querySelector('.bio-container')
     bioContainer.insertAdjacentHTML('beforeend', bioInfo)
@@ -90,24 +68,13 @@ function artistBio(data) { /*  add artist bio to .bio-container */
 
 
 // COMPONENT 5: RESET DOM / REMOVE LAST
-function removeBgImg() {  
-    const rmArt = document.querySelector('.main-container')
-    while (rmArt.lastChild) {
-        rmArt.removeChild(rmArt.lastChild)
+function removeMain() {  
+    const rmMain = document.querySelector('.main-container')
+    while (rmMain.lastChild) {
+        rmMain.removeChild(rmMain.lastChild)
     }
 }
-function removeDetails() { 
-    const rmDetails = document.querySelector('.details-container')
-    while (rmDetails.lastChild) {
-        rmDetails.removeChild(rmDetails.lastChild)
-    }
-}
-function removeThumbnail() { 
-    const rmThumbnail = document.querySelector('.details-container')
-    while (rmThumbnail.lastChild) {
-        rmThumbnail.removeChild(rmThumbnail.lastChild)
-    }
-}
+
 function removeBio() { 
     const rmBio = document.querySelector('.bio-container')
     while (rmBio.lastChild) {
